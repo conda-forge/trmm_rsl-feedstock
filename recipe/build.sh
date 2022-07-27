57a4877a970137c73810a9a64461942d2345d1ab
mgrover1 <maxi2312@gmail.com>
2022-07-27 15:59:52 -0500

    add back in build without make

---
+++ recipe/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ `uname` == Linux ]; then
+    # Enable -z def linked flag
+    # linking will fail when undefined symbols are present
+    export CFLAGS="${CFLAGS} -Wl,-z,defs"
+fi
+chmod +x configure
+./configure  --prefix=$PREFIX
