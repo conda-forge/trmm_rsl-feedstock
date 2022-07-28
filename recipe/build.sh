6898e43c9f5153b524d74683147b4cf6df3ee7b1
mgrover1 <maxi2312@gmail.com>
2022-07-28 07:14:46 -0500

    remove if then block

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if [ `uname` == Linux ]; then
-    # Enable -z def linked flag
-    # linking will fail when undefined symbols are present
-    export CFLAGS="${CFLAGS} -Wl,-z,defs"
-fi
+# Get an updated config.sub and config.guess
+cp $BUILD_PREFIX/share/gnuconfig/config.sub .
+cp $BUILD_PREFIX/share/gnuconfig/config.guess .
+
+
 chmod +x configure
 ./configure  --prefix=$PREFIX
 make
