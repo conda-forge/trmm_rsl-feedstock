ad09f65f9acf5ba68a415c351dc0b6be11f4a01b
mgrover1 <maxi2312@gmail.com>
2022-08-01 14:43:22 -0500

    use the optional flags variable

--- recipe/build.sh
+++ recipe/build.sh
@@ -12,7 +12,7 @@ elif [ `uname` == osx ]; then
     export L="/usr/local/lib"
 fi
 echo "${CFLAGS}"
-export CFLAGS="${CFLAGS} -std=c90"
+export CC_OPT_FLAGS="-std=c90,-Wno-implicit-function-declaration"
 chmod +x configure
 ./configure --prefix=$PREFIX
 make
