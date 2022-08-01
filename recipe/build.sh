4793a4efa253641ff922a30b11b23056a9890055
mgrover1 <maxi2312@gmail.com>
2022-08-01 11:36:53 -0500

    add cflag inside if then

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,9 +7,11 @@ if [ `uname` == Linux ]; then
     # Enable -z def linked flag
     # linking will fail when undefined symbols are present
     export CFLAGS="${CFLAGS} -Wl,-z,defs"
+elif [ `uname` == osx ]; then
+    export CFLAGS="${CFLAGS} --std=c90 -Wno-implicit-function-declaration"
 fi
 echo "${CFLAGS}"
 chmod +x configure
-./configure CFLAGS="--std=c90 -Wno-implicit-function-declaration" --prefix=$PREFIX
+./configure --prefix=$PREFIX
 make
 make install
