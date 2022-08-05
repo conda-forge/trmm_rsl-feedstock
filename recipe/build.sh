cddcab15b2652531ecbf29d6671f7b4caf1cb145
Max Grover <mgroverwx@gmail.com>
2022-08-05 12:52:01 -0500

    add sudo apt install to build

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,6 +3,7 @@
 if [ `uname` == Linux ]; then
     # Enable -z def linked flag
     # linking will fail when undefined symbols are present
+    sudo apt-get install libbz2-dev
     export CFLAGS="${CFLAGS} -Wl,-z,defs"
 fi
 chmod +x configure
