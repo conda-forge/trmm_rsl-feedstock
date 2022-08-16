a8f57a4d3132498b0cb6f4c3bce7d5855afa96bd
mgrover1 <mgroverwx@gmail.com>
2022-08-16 12:09:09 -0500

    add another cflag

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,7 +3,7 @@
 if [ `uname` == Linux ]; then
     # Enable -z def linked flag
     # linking will fail when undefined symbols are present
-    export CFLAGS="${CFLAGS} -Wl,-z,defs"
+    export CFLAGS="${CFLAGS} -Wl,-z,defs,-Wno-implicit-function-declaration"
 fi
 chmod +x configure
 ./configure  --prefix=$PREFIX
